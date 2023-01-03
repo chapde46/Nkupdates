4860202c63670f5e99d4c01f05325f516fdc90f7
Shree <Shree@DESKTOP-I4E911K.bbrouter>
2023-01-03 08:31:31 +0530

    newyear[change]

--- src/test/java/TestLogin/Login2Naukari.java
+++ src/test/java/TestLogin/Login2Naukari.java
@@ -42,7 +42,7 @@ public class Login2Naukari extends InvokeBrowser {
 
 		UpProfile.clickOnUpdateProfile();
 		// Daily update the path of resume and just commit and push it to github daily
-		String resumepath = "C:\\Users\\Shree\\Music\\Vishal B Chapde._Banglore_Final2022.pdf";
+		String resumepath = "C:\\Users\\Shree\\Pictures\\BlueStacks\\Vishal B Chapde._Banglore_Final2022.pdf";
 		uploadRusm.uploadresume(wait, resumepath);
 
 		UpProfile2.ClickUpdateResumeHeadline("Resume Headline");
