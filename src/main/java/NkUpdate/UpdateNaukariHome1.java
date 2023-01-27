ebfb24015c9f813c598af5a8507923fd0103dd7a
Shree <Shree@DESKTOP-I4E911K.bbrouter>
2023-01-27 10:02:18 +0530

    [update janewary]27

--- src/main/java/NkUpdate/UpdateNaukariHome1.java
+++ src/main/java/NkUpdate/UpdateNaukariHome1.java
@@ -7,17 +7,27 @@ import org.openqa.selenium.support.PageFactory;
 
 public class UpdateNaukariHome1 {
 
-	@FindBy(css = "div[class='updateProfile'] div[class='mb10'] div")
+	@FindBy(xpath = "//div[@class='updateProfile']/div[@class='mb10']/div")
 	private WebElement updateBtn;
+	@FindBy(xpath = "//div[@class='chatbot_DrawerContentWrapper']")
+	private WebElement chatbot;
+	@FindBy(xpath = "//div[@class='chatbot_DrawerContentWrapper']/div/div[contains(@class,'crossIcon')]")
+	private WebElement closeChatbotBtn;
 
 	public UpdateNaukariHome1(WebDriver driver) {
 
 		PageFactory.initElements(driver, this);
 	}
+	
+	
 
 	public void clickOnUpdateProfile() throws InterruptedException {
-
-		Thread.sleep(1500);
+		Thread.sleep(2000);
+		if(chatbot.isDisplayed()) {
+			closeChatbotBtn.click();
+		}	
+		
+		Thread.sleep(2000);
 		updateBtn.click();
 
 	}
