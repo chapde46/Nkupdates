30939cb7f95c7ae692a6219cfb35f7592c9941c0
Shree <Shree@DESKTOP-I4E911K.bbrouter>
2023-03-09 08:14:55 +0530

    [update march 9]

--- src/main/java/NkBase/InvokeBrowser.java
+++ src/main/java/NkBase/InvokeBrowser.java
@@ -4,19 +4,23 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class InvokeBrowser {
 
+	
 	public WebDriver driver;
 	public WebDriverWait wait;
-
+	
+	
 	public void browserOpen() {
-
+		ChromeOptions ops = new ChromeOptions();
+		ops.addArguments("--remote-allow-origins=*");
 		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		driver=new ChromeDriver(ops);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		driver.manage().window().maximize();
